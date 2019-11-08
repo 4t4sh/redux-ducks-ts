7e678c982bb0b70a0f1679e3049db5285142e2b0
Ata Sanchez <atasanchez@Atas-MacBook-Air.local>
2019-11-08 09:47:04 -0300

    Update Octobre 2019

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,45 @@
-import Ducks from "./models/Duck";
+// Types
+import { DuckAction } from "./types";
 
-export default Ducks;
+export default class ModuleDuck {
+  appName: string;
+  moduleName: string;
+
+  constructor(module: string, app: string) {
+    this.moduleName = module;
+    this.appName = app;
+  }
+
+  public defineType = (type: string): string =>
+    this.appName
+      ? `${this.appName}/${this.moduleName}/${type}`
+      : `${this.moduleName}/${type}`;
+
+  public createAction = (type: string) => (payload?: any): DuckAction => {
+    const action: DuckAction = {
+      type,
+      payload
+    };
+
+    return action;
+  };
+
+  public createReducer = (
+    cases: {
+      [key: string]: any;
+    },
+    initialState: object = {}
+  ) => (state?: object, action?: DuckAction): object => {
+    if (state === undefined) return initialState;
+
+    if (action !== undefined) {
+      for (const caseName in cases) {
+        if (action.type === caseName) return cases[caseName](state, action);
+      }
+    }
+
+    return state;
+  };
+}
+
+export { DuckAction };
